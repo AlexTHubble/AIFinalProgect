f9add84c8ed005fce3e6a95bc93830ec874a1967
Josh Grazda <joshuagrazda@yahoo.com>
2018-12-11 22:28:06 -0500

    Uncommented stuff

--- Assigment 3/GameAI/pathfinding/game/Collision.cpp
+++ Assigment 3/GameAI/pathfinding/game/Collision.cpp
@@ -44,7 +44,7 @@ void Collision::CheckForCollisions(Unit* unit)
 					gpGame->getUnitManager()->setToDeleteUnit(unitIter->second->getID());
 				}
 			}
-			//if (unitIter->second->getTag() == "PowerUp")
+			if (unitIter->second->getTag() == "PowerUp")
 			{
 				if (HitPowerUp(unit, unitIter->second))
 				{
